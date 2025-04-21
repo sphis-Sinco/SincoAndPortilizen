7c8417bbba9311c382c5f17d028cc7eefba23875
Sphis-Sinco <sinconsistencia@gmail.com>
2025-04-20 21:57:54 -0500

    Basic bitch title state

---
+++ source/sap/menus/TitleState.hx
@@ -0,0 +1,12 @@
+package sap.menus;
+
+import sap.backends.State;
+
+class TitleState extends State
+{
+
+        override public function new() {
+                super('TitleState');
+        }
+        
+}

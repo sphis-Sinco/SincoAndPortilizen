85a4a044d9c60129a9eccd2991f1b9cccb8502ce
Sphis-Sinco <sinconsistencia@gmail.com>
2025-04-21 16:44:44 -0500

    character ring

--- source/sap/menus/TitleState.hx
+++ source/sap/menus/TitleState.hx
@@ -1,30 +1,41 @@
 package sap.menus;
 
-enum abstract TitleSequences(String) from String to String {
-        var DEBUG = 'debug';
-        var INTRO = 'intro';
-        var FLASH = 'flash';
-        var COMPLETE = 'complete';
+enum abstract TitleSequences(String) from String to String
+{
+	var DEBUG = 'debug';
+	var INTRO = 'intro';
+	var FLASH = 'flash';
+	var COMPLETE = 'complete';
 }
 
 class TitleState extends State
 {
-        public static var SEQUENCE:TitleSequences = (Global.DEBUG_BUILD) ? DEBUG : INTRO;
+	public static var SEQUENCE:TitleSequences = (Global.DEBUG_BUILD) ? DEBUG : INTRO;
 
-        override public function new() {
-                super('TitleState');
-        }
+        public static var CHARACTER_RING:SAPSprite;
 
-        override function create() {
-                super.create();
+	override public function new()
+	{
+		super('TitleState');
+	}
 
-                var backdrop:SAPSprite = new SAPSprite({
+	override function create()
+	{
+		super.create();
+
+		var backdrop:SAPSprite = new SAPSprite({
+			position: [0, 0],
+			imagePath: 'titlescreen/TitleBG',
+			scaleAddition: 1
+		});
+		add(backdrop);
+		backdrop.screenCenter();
+
+                CHARACTER_RING = new SAPSprite({
                         position: [0,0],
-                        imagePath: 'titlescreen/TitleBG',
-                        scaleAddition: 1
+                        imagePath: 'titlescreen/CharacterRing'
                 });
-                add(backdrop);
-                backdrop.screenCenter();
-        }
-        
+                add(CHARACTER_RING);
+                CHARACTER_RING.screenCenter();
+	}
 }
